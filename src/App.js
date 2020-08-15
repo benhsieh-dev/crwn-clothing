18f187bf081bb824428419e526b1c68920060bc2
Ben Hsieh <bqh5026@gmail.com>
2020-08-14 23:25:24 -0400

    Integrate redux-saga for signin process

--- src/App.js
+++ src/App.js
@@ -11,35 +11,30 @@ import SingInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
-import { 
-  auth, 
-  createUserProfileDocument 
-  } from './firebase/firebase.utils'; 
 
-import { setCurrentUser } from './redux/user/user.actions'; 
 import { selectCurrenttUser } from './redux/user/user.selectors';
+import { checkUserSession} from './redux/user/user.actions'; 
 
 class App extends React.Component {
   unsubscribeFromAuth = null 
 
   componentDidMount() {
-    const { setCurrentUser } = this.props; 
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    //   if (userAuth) {
+    //     const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-            });
-        }); 
-      }
+    //     userRef.onSnapshot(snapShot => {
+    //       setCurrentUser({
+    //           id: snapShot.id,
+    //           ...snapShot.data()
+    //         });
+    //     }); 
+    //   }
    
-        setCurrentUser(userAuth); 
+    //     setCurrentUser(userAuth); 
       
-    }); 
+    // }); 
   }
 
   componentWillUnmount() {
@@ -71,15 +66,13 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = createStructuredSelector({
+const msp = createStructuredSelector({
   currentUser: selectCurrenttUser,       
 }); 
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
+const mdp = dispatch => ({
+  checkUserSession: () => dispatch(checkUserSession())
+}); 
 
-export default connect(
-  mapStateToProps, 
-  mapDispatchToProps
-  )(App);
+
+export default connect(msp, mdp)(App);
