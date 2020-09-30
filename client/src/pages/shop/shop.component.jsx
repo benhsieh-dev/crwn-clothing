45e471e1f4dc56b1df6e155f2ed56d68afdffa29
Ben Hsieh <bqh5026@gmail.com>
2020-09-30 13:41:21 -0400

    Edit CollectionsOverviewContainer to inpurt from the correct path

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions.js';
 
-import CollectionsOverviewContainer from '../../components/collectios-overview/collections-overview.container';
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
 
